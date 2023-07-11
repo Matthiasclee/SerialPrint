8a1379eba21cfdf9e8bf62e336e339b2dba7906c
Matthias Lee <matthias@matthiasclee.com>
2023-07-11 16:18:48 -0700

    Read data all at once

--- lib/serialprint/serial.rb
+++ lib/serialprint/serial.rb
@@ -3,9 +3,15 @@ module SerialPrint
     def self.initialize(port, baud:9600)
       @ser = SerialPort.new(port, baud, 8, 1, SerialPort::NONE)
 
+      data = ""
+
       loop do
-        STDOUT.print @ser.read(1)
+        byte = @ser.read(1)
+        break if byte == "\f"
+        data << byte
       end
+
+      STDOUT.puts data
     end
   end
 end
