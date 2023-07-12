ccfd110229fd46247ef0e988782aca53cd5e8db2
Matthias Lee <matthias@matthiasclee.com>
2023-07-12 09:21:49 -0700

    Misc

--- windows/copy_to_startup.bat
+++ windows/copy_to_startup.bat
@@ -1,1 +1,3 @@
-xcopy start_hidden.vbs "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_serialprint.vbs"
+@echo off
+
+xcopy start_hidden.vbs "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_serialprint.vbs" /I
