683c5be90761f32e38fa641dcd869aea080aa2c0
Matthias Lee <matthias@matthiasclee.com>
2023-07-12 08:28:44 -0700

    Ps1

--- windows/install_gems.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-start gem update --system --no-document
-start gem install ffi --no-document
-start gem install serialport --no-document
---
+++ windows/install_gems.ps1
@@ -0,0 +1,3 @@
+gem update --system --no-document
+gem install ffi --no-document
+gem install serialport --no-document
