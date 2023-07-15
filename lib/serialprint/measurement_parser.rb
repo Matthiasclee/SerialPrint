d1b20f0c7ab3551290196f3a73edfdbce5079698
Matthias Lee <matthias@matthiasclee.com>
2023-07-15 15:24:24 -0700

    More parsing

--- lib/serialprint/measurement_parser.rb
+++ lib/serialprint/measurement_parser.rb
@@ -5,7 +5,49 @@ module SerialPrint
       measurement_data.delete_at(0)
       measurement_data.delete_at(0)
 
-      return measurement_data.join("\r\n")
+      od_data = [
+        [],
+        [],
+        [],
+        [],
+        []
+      ]
+
+      os_data = [
+        [],
+        [],
+        [],
+        [],
+        []
+      ]
+
+      od_avg_data = []
+      os_avg_data = []
+
+
+      measurement_data.each_with_index do |line, iteration|
+        od = line.split(" ")[0..4].map{|x|x.to_i}
+        os = line.split(" ")[5..9].map{|x|x.to_i}
+
+        od.each_with_index do |m, i|
+          od_data[i][iteration]=m
+        end
+
+        os.each_with_index do |m, i|
+          os_data[i][iteration]=m
+        end
+
+        od_avg_data = od.sum / 5
+        os_avg_data = os.sum / 5
+      end
+
+
+      return {
+        od: od_data,
+        os: os_data,
+        od_avg: od_avg_data,
+        os_avg: os_avg_data
+      }
     end
   end
 end
